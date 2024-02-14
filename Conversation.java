6fa93bd4872e39a76378a332062afdc53e6caaa7
kat-brady <kbrady@smith.edu>
2024-02-13 21:34:59 -0500

    finished

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,73 @@
-class Conversation {
+import java.util.*;
+public class Conversation { //the chatbot code
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How many rounds of conversation?");
+    int Rounds= sc.nextInt(); //this saves the user's input as Rounds
+    sc.nextLine();
+    List<String> transcript= new ArrayList<String>(); //this creates the transcript in order to later print it
+    transcript.add("TRANSCRIPT:\n");
+    transcript.add("How many rounds of conversation?");
+    String times=String.valueOf(Rounds); //this converts the integer Rounds into a string so that it can be added to the transcript
+    transcript.add(times);
+    transcript.add("Hello! What are you thinking about today?");
+    System.out.println("Hello! What are you thinking about today?");
+    //The intro is printed once before the loop starts
+    for (int i=1; i<= Rounds; i++){ //this allows the chat to continue the desired number of rounds
+      Random rand= new Random(); //generates a random number to use later for a canned response
+        int num = rand.nextInt(1,4);
+      String response = sc.nextLine();
+      transcript.add(response);
+      if (response.contains("I")||response.contains("you")||response.contains("your")||response.contains("me")||response.contains("myself")||response.contains("am")||response.contains("my")){
+        //the words are replaced with temporary words to prevent issues with the replacement words being overwritten
+        response= response.replaceAll("I", "tempY"); //tempY -> you
+        response = response.replaceAll("your", "tempP");//tempP -> my
+        response = response.replaceAll("you", "tempI");//tempI -> I
+        response = response.replaceAll("me", "tempY");//tempY -> you
+        response = response.replaceAll("myself", "SELF");//SELF-> yourself
+        response = response.replaceAll("am", "tempR");//tempR -> are
+        response = response.replaceAll("are", "tempAM");//tempAM -> am
+        response = response.replaceAll("my", "tempUR");//tempUR -> your
+        response= response.replaceAll("tempY", "you");
+        response = response.replaceAll("tempI", "I");
+        response = response.replaceAll("tempP", "my");
+        response = response.replaceAll("tempY", "you");
+        response = response.replaceAll("SELF", "yourself");
+        response = response.replaceAll("tempR", "are");
+        response = response.replaceAll("tempAM", "am");
+        response = response.replaceAll("tempUR", "your");
+        response = response.replaceAll("'m", "tempAP");
+        response = response.replaceAll("'re", "TEMPRE");
+        response = response.replaceAll("tempAP", "'re");
+        response = response.replaceAll("TEMPRE", "'m");
+        System.out.println(response + "?");
+        transcript.add(response + "?");}
+      else{ //if none of the mirror words are detected, a canned response is used instead
+        if (num==1){
+          System.out.println("That's wild!");
+        transcript.add("That's wild!");}
+        else if (num==2){
+          System.out.println("Wow, tell me more!");
+          transcript.add("Wow, tell me more!");}
+          else if (num==3){
+          System.out.println("I'm totally listening.");
+          transcript.add("I'm totally listening.");}
+        else if (num==4){
+          System.out.println("This is such a fun conversation.");
+          transcript.add("This is such a fun conversation.");}
+        else{
+          System.out.println("Oh, okay.");
+          transcript.add("Oh, okay.");}
+    }
+  }
+  String bye="It was nice talking to you. Bye now!\n"; //this wraps up the conversation
+    transcript.add(bye);
+    System.out.println(bye);
+    for(String line: transcript){
+      System.out.println(line);
+    }
+      sc.close(); //the scanner is closed
   }
 }
+
--- reflection.md
+++ reflection.md
@@ -5,3 +5,10 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+
+I did not consult with classmates in person on this assignment due to my schedule this week, but I engaged with some classmates on discord. I attended TA hours on February 13th to finalize my code. Talking to people in various mediums made me realize that Java is simpler than it may have seemed at first. Initially, I don't really have a lot of thoughts on Java. It isn't harder or easier than Python as far as I can tell, just different. I kind of like it so far. In terms of conclusions, I think that maybe all programming languages may be different, but it is possible to do the same sorts of things with them. That is to say that there is more than one path to the end goal (like, I could've coded this assignment in Python as well). 
+
+What worked best for me was probably just using online resources (specifically W3 schools) to read about Java syntax rather than just being confused or feeling the need to admit defeat. As long as I know what to look for, I can handle a lot of debugging and looking for syntax on my own. I would advise someone to come into this assignment with an open mind and be patient with themselves.
+
+I will say that I am still confused on what counts as a "round" of conversation, but I set my code to run according to what seemed like a round to me. That isn'ta coding issue so much as it is an issue of clarity of instructions.

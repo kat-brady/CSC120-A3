cd025fb6972b47ebc0ed1062d356568c48c8239e
kat-brady <kbrady@smith.edu>
2024-04-16 14:14:08 -0400

    edited

--- Conversation.java
+++ Conversation.java
@@ -1,7 +1,18 @@
 import java.util.*;
-public class Conversation { //the chatbot code
-
+/*The Conversation class contains all of the methods for the chatbot to function */
+public class Conversation { 
+    /* main method for the Conversation class to run the chatbot
+     * Initializes a scanner to get user input
+     * Initializes a transcript to display to the user
+     * Mirrors responses or produces response from array of canned ones for desired number of rounds
+     */
   public static void main(String[] args) {
+    ArrayList<String> cannedResponses = new ArrayList<>();
+    cannedResponses.add("That's wild!");
+    cannedResponses.add("I'm totally listening.");
+    cannedResponses.add("Wow, tell me more.");
+    cannedResponses.add("This is such a fun conversation.");
+    cannedResponses.add("Oh, okay.");
     Scanner sc = new Scanner(System.in);
     System.out.println("How many rounds of conversation?");
     int Rounds= sc.nextInt(); //this saves the user's input as Rounds
@@ -19,46 +30,35 @@ public class Conversation { //the chatbot code
         int num = rand.nextInt(1,4);
       String response = sc.nextLine();
       transcript.add(response);
-      if (response.contains("I")||response.contains("you")||response.contains("your")||response.contains("me")||response.contains("myself")||response.contains("am")||response.contains("my")){
+      if (response.contains("\\bI\\b")||response.contains("\\byou\\b")||response.contains("\\byour\\b")||response.contains("\\bme\\b")||response.contains("\\bmyself\\b")||response.contains("\\bam\\b")||response.contains("\\bmy\\b")){
         //the words are replaced with temporary words to prevent issues with the replacement words being overwritten
-        response= response.replaceAll("I", "tempY"); //tempY -> you
-        response = response.replaceAll("your", "tempP");//tempP -> my
-        response = response.replaceAll("you", "tempI");//tempI -> I
-        response = response.replaceAll("me", "tempY");//tempY -> you
-        response = response.replaceAll("myself", "SELF");//SELF-> yourself
-        response = response.replaceAll("am", "tempR");//tempR -> are
-        response = response.replaceAll("are", "tempAM");//tempAM -> am
-        response = response.replaceAll("my", "tempUR");//tempUR -> your
-        response= response.replaceAll("tempY", "you");
-        response = response.replaceAll("tempI", "I");
-        response = response.replaceAll("tempP", "my");
+        response= response.replaceAll("\\bI\\b", "tempY"); //tempY -> you
+        response = response.replaceAll("\\byour\\b", "tempP");//tempP -> my
+        response = response.replaceAll("\\byou\\b", "tempI");//tempI -> I
+        response = response.replaceAll("\\bme\\b ", "tempY");//tempY -> you
+        response = response.replaceAll("\\bmyself\\b", "SELF");//SELF-> yourself
+        response = response.replaceAll("\\bam\\b", "tempR");//tempR -> are
+        response = response.replaceAll("\\bare\\b", "tempAM");//tempAM -> am
+        response = response.replaceAll("\\bmy\\b", "tempUR");//tempUR -> your
         response = response.replaceAll("tempY", "you");
-        response = response.replaceAll("SELF", "yourself");
-        response = response.replaceAll("tempR", "are");
-        response = response.replaceAll("tempAM", "am");
-        response = response.replaceAll("tempUR", "your");
-        response = response.replaceAll("'m", "tempAP");
+        response = response.replaceAll("tempI", " I ");
+        response = response.replaceAll("tempP", " my ");
+        response = response.replaceAll("tempY", " you ");
+        response = response.replaceAll("SELF", " yourself ");
+        response = response.replaceAll("tempR", " are ");
+        response = response.replaceAll("tempAM", " am ");
+        response = response.replaceAll("tempUR", " your ");
+        response = response.replaceAll("'m", " tempAP ");
         response = response.replaceAll("'re", "TEMPRE");
         response = response.replaceAll("tempAP", "'re");
         response = response.replaceAll("TEMPRE", "'m");
         System.out.println(response + "?");
         transcript.add(response + "?");}
       else{ //if none of the mirror words are detected, a canned response is used instead
-        if (num==1){
-          System.out.println("That's wild!");
-        transcript.add("That's wild!");}
-        else if (num==2){
-          System.out.println("Wow, tell me more!");
-          transcript.add("Wow, tell me more!");}
-          else if (num==3){
-          System.out.println("I'm totally listening.");
-          transcript.add("I'm totally listening.");}
-        else if (num==4){
-          System.out.println("This is such a fun conversation.");
-          transcript.add("This is such a fun conversation.");}
-        else{
-          System.out.println("Oh, okay.");
-          transcript.add("Oh, okay.");}
+        Random rand = new Random();
+        reply=cannedResponses.get(rand);
+        System.out.println(reply);
+        transcript.add(reply);
     }
   }
   String bye="It was nice talking to you. Bye now!\n"; //this wraps up the conversation
